000c417ea5a70d7e23e49713b954be5e126a1912
devnahin <dev.nahin@gmail.com>
2024-10-13 19:11:39 +0600

    Update
    Remove language file

--- post-grid-shortcode.php
+++ post-grid-shortcode.php
@@ -9,7 +9,6 @@
  * Text Domain: post-grid-shortcode
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Domain Path: /languages
  */
 
 defined('ABSPATH') || exit; // Exit if accessed directly
